99e7255918ff5cc4973dd0c340011501d4e5a58a
KOEGlike <koeglike@proton.me>
2025-11-08 15:10:43 +0200

    Update Scene component with new sprite and enhanced scene setup; add Ellipse 1.svg

--- src/routes/Scene.svelte
+++ src/routes/Scene.svelte
@@ -2,7 +2,7 @@
 	import { T, useTask } from '@threlte/core';
 	import { Align, interactivity, OrbitControls, PointsMaterial } from '@threlte/extras';
 	import { Spring } from 'svelte/motion';
-	import { BufferGeometry, SRGBColorSpace, TextureLoader } from 'three';
+	import { BufferGeometry, Color, Fog, SRGBColorSpace, TextureLoader } from 'three';
 
 	interactivity();
 	const scale = new Spring(1);
@@ -12,11 +12,11 @@
 		rotation += delta;
 	});
 
-	const sprite = new TextureLoader().load('disc.png');
+	const sprite = new TextureLoader().load('Ellipse 1.svg');
 	//2sprite.colorSpace = SRGBColorSpace;
 
-	const spacing = 1;
-	const count = 10;
+	const spacing = 5;
+	const count = 100;
 
 	const total = count * count * count;
 	const positions = new Float32Array(total * 3);
@@ -37,60 +37,63 @@
 	}
 </script>
 
-<T.PerspectiveCamera
-	makeDefault
-	fov={50}
-	position={[10, 10, 10]}
-	oncreate={(ref) => {
-		ref.lookAt(0, 1, 0);
-	}}
->
-	<OrbitControls enableDamping />
-</T.PerspectiveCamera>
+<T.Scene background={new Color().setHex(0x3d3d3d)} fog={new Fog(0x3d3d3d, 10, 1000)}>
+	<T.PerspectiveCamera
+		makeDefault
+		fov={50}
+		position={[10, 10, 10]}
+		oncreate={(ref) => {
+			ref.lookAt(0, 1, 0);
+		}}
+	>
+		<OrbitControls enableDamping />
+	</T.PerspectiveCamera>
 
-<T.DirectionalLight position={[0, 10, 10]} castShadow />
+	<T.DirectionalLight position={[0, 10, 10]} castShadow />
 
-<T.Mesh
-	rotation.y={rotation}
-	position.y={1}
-	scale={scale.current}
-	onpointerenter={() => {
-		scale.target = 1.5;
-	}}
-	onpointerleave={() => {
-		scale.target = 1;
-	}}
-	castShadow
->
-	<T.BoxGeometry args={[1, 2, 1]} />
-	<T.MeshStandardMaterial color="hotpink" />
-</T.Mesh>
+	<T.Mesh
+		rotation.y={rotation}
+		position.y={1}
+		scale={scale.current}
+		onpointerenter={() => {
+			scale.target = 1.5;
+		}}
+		onpointerleave={() => {
+			scale.target = 1;
+		}}
+		castShadow
+	>
+		<T.BoxGeometry args={[1, 2, 1]} />
+		<T.MeshStandardMaterial color="hotpink" />
+	</T.Mesh>
 
-<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
-	<T.CircleGeometry args={[4, 40]} />
-	<T.MeshStandardMaterial color="white" />
-</T.Mesh>
+	<T.Mesh rotation.x={-Math.PI / 2} receiveShadow>
+		<T.CircleGeometry args={[4, 40]} />
+		<T.MeshStandardMaterial color="white" />
+	</T.Mesh>
 
-<Align>
-	<T.Points>
-		<T.BufferGeometry>
-			<T.BufferAttribute
-				args={[positions, 3]}
-				attach={({ parent, ref }) => {
-					(parent as BufferGeometry).setAttribute('position', ref);
-					return () => {
-						// cleanup function called when ref changes or the component unmounts
-						// https://threlte.xyz/docs/reference/core/t#attach
-					};
-				}}
+	<Align>
+		<T.Points>
+			<T.BufferGeometry>
+				<T.BufferAttribute
+					args={[positions, 3]}
+					attach={({ parent, ref }) => {
+						(parent as BufferGeometry).setAttribute('position', ref);
+						return () => {
+							// cleanup function called when ref changes or the component unmounts
+							// https://threlte.xyz/docs/reference/core/t#attach
+						};
+					}}
+				/>
+			</T.BufferGeometry>
+			<T.PointsMaterial
+				size={0.25}
+				map={sprite}
+				transparent={true}
+				alphaTest={0.1}
+				depthWrite={false}
+				sizeAttenuation={true}
 			/>
-		</T.BufferGeometry>
-		<T.PointsMaterial
-			size={0.25}
-			map={sprite}
-			transparent={true}
-			alphaTest={0.1}
-			depthWrite={false}
-		/>
-	</T.Points>
-</Align>
+		</T.Points>
+	</Align>
+</T.Scene>
