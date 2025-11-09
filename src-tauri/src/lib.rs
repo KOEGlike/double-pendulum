4231f15d4b74522170eb8b9c578d7a3eadbc9ab2
KOEGlike <koeglike@proton.me>
2025-11-09 19:14:38 +0200

    Refactor Tauri command structure and enhance data models for pendulum simulation

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,45 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Heeello, {}! You've been greeted from Rust!", name)
+use std::sync::Mutex;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Coordinate {
+    x: f64,
+    y: f64,
 }
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct Bob {
+    length_rod: f64,
+    mass: f64,
+    theta: f64,
+    omega: f64,
+    coordinate: Coordinate,
+}
+#[derive(Clone, Debug, PartialEq)]
+struct Pendulum {
+    g: f64,
+    origin: Coordinate,
+    bobs: Vec<Bob>,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+
+struct AppDataInner {
+    pendulum: Pendulum,
+}
+
+type AppData = Mutex<AppDataInner>;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {})
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
 }
